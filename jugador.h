cff6678f1b2fafeb73f4a145d29bdd9542ea4972
Pablo García Sánchez <fergunet@gmail.com>
2015-11-27 10:48:48 +0100

    He creado este fichero tan hax0r

---
+++ jugador.h
@@ -0,0 +1,14 @@
+lgjdfklg jdf ilñfñf yfsfsdr vwj rpe irwe 
+fsfasdjlfhads jkfh for (int i = 0; i < sdfauoifjfasf; ++i)
+{
+	/* code */
+}
+joasd foiw jfñsa lksa f sd sdfauoifjfasffasfsd
+fsafs sa
+fweru084fsd
+
+fsfofr
+for (int i = 0; i < jfjewljtj; ++i)
+{
+	/* code */
+}
